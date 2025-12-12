f792b1b62c33a0ee4850f4a843f891b370e61937
louayamor <amor.louay20@gmail.com>
2025-12-12 10:19:35 +0100

    test: test dvc

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from Reasona.pipeline.preprocess_pipeline import PreprocessPipeline
 ##from Reasona.pipeline.training_pipeline import TrainingPipeline
 ##from Reasona.pipeline.inference_pipeline import InferencePipeline
 
-# Main pipeline logger
 logger = setup_logger("main_pipeline", "logs/pipeline/main_pipeline.log")
 
 def run_stage(stage_name, pipeline_cls):
-    # Stage-specific logger (optional)
     stage_logger = setup_logger(f"{stage_name.lower()}_stage", f"logs/pipeline/{stage_name.lower()}_stage.log")
     stage_logger.info(f"===== Starting: {stage_name} =====")
     try:
         pipeline = pipeline_cls()
-        pipeline.run()  # Submodules log to their own loggers
+        pipeline.run()  
         stage_logger.info(f"===== Completed: {stage_name} =====\n")
     except Exception as e:
         stage_logger.exception(f"Error during {stage_name}: {e}")

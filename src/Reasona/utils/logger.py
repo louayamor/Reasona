0d54a4e0c70900c02eb8145bddbe08dd6ebaf106
louayamor <amor.louay20@gmail.com>
2026-01-12 22:42:58 +0100

    refactor: removed json logging for more readable logs

--- src/Reasona/utils/logger.py
+++ src/Reasona/utils/logger.py
@@ -1,6 +1,5 @@
 import logging
 from pathlib import Path
-from pythonjsonlogger import jsonlogger
 
 def setup_logger(logger_name: str, log_path: str):
     log_file = Path(log_path)
@@ -10,16 +9,20 @@ def setup_logger(logger_name: str, log_path: str):
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
+        
         fh = logging.FileHandler(log_file, mode="a", encoding="utf-8")
         fh.setLevel(logging.INFO)
-        json_formatter = jsonlogger.JsonFormatter(
-            '%(asctime)s %(name)s %(levelname)s %(message)s'
+        file_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
-        fh.setFormatter(json_formatter)
+        fh.setFormatter(file_formatter)
 
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
-        ch.setFormatter(json_formatter)
+        console_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        ch.setFormatter(console_formatter)
 
         logger.addHandler(fh)
         logger.addHandler(ch)
